d90e270d676e68603e6b66fe21644ecbb2a022de
Jacob Feng <peanutChowder@gmail.com>
2024-04-05 09:40:07 -0600

    added byte per pixel calc and some of defiltering

--- src/processImage.cpp
+++ src/processImage.cpp
@@ -47,3 +47,57 @@ bool decompressIDAT(const std::vector<unsigned char>& compressedData, std::vecto
     std::reverse(decompressedData.begin(), decompressedData.end());
     return true;
 }
+
+bool defilterIDAT(std::vector<unsigned char> &decompressedData, int width, int height, int colorType, int channelDepth) {
+    int bytesPerPixel, colWidth;
+    int filter;
+
+    if ((bytesPerPixel = getBytesPerPixel(colorType, channelDepth)) == -1) {
+        return false;
+    }
+
+    // each line is occupied by pixel data + 1 byte for the filter
+    colWidth = width * bytesPerPixel + 1;
+
+    for (int lineIndex = 0; lineIndex < height; lineIndex++) {
+        filter = decompressedData[lineIndex * colWidth]; // get the filter which is located in the first byte of each line
+
+        for (int colIndex = 0; colIndex < colWidth; colIndex++) {
+            // TODO
+        }
+    }
+
+    return true;
+}
+
+int getBytesPerPixel(int colorType, int channelDepth) {
+    int bytesPerChannel;
+
+    if (channelDepth < 8) {
+        std::cerr << "Channel depths less than 1 byte currently unsupported" << std::endl;
+        return -1;
+    }
+
+    bytesPerChannel = channelDepth / 8;
+
+    switch (colorType) {
+        // greyscale image, 1 channel per pixel
+        case 0:
+            return bytesPerChannel * 1;
+        // Truecolor image, 3 channels per pixel (RGB)
+        case 2:
+            return bytesPerChannel * 3;
+        // Indexed color image, 1 channel per pixel (need indexing)
+        case 3:
+            return bytesPerChannel * 1;
+        // Greyscale with alpha, 2 channels per pixel
+        case 4:
+            return bytesPerChannel * 2;
+        // Truecolor with alpha, 4 channels per pixel (RGBA)
+        case 6:
+            return bytesPerChannel * 4;
+        default:
+            std::cerr << "Unsupported color type '" << colorType << "'." << std::endl;
+            return -1;
+    }
+}
--- src/processImage.h
+++ src/processImage.h
@@ -1,3 +1,5 @@
 #include <vector>
 
 bool decompressIDAT(const std::vector<unsigned char>& compressedData, std::vector<unsigned char> &decompressedData);
+
+int getBytesPerPixel(int colorType, int channelDepth);
